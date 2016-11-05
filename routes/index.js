280f17b6339c40fbc6c58afc9c9ed2269e3a3eea
Geoffrey Long <Geoffrey.Long@mail.mcgill.ca>
2016-11-05 02:10:59 -0400

    ALTER callback from passport authenticate

--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,30 @@ router.get('/', function(req, res) {
   res.sendFile(path.resolve('index.html'));
 });
 
-// This may reload the page... I don't think we want that behavior
-router.post('/login', passport.authenticate('local', 
-            { successRedirect: '/', failureRedirect: '/login' }));
 
+router.post('/login', function(req, res) {
+  passport.authenticate('local', function(err, user, info) {
+    if (err) { 
+      console.log(err);
+      console.log("case 1");
+      return res.status(400).send(err);
+    }
+    if (!user) { 
+      console.log("case 2");
+      return res.status(400).send(""); 
+    }
+    req.logIn(user, function(err) {
+      if (err) { 
+        console.log("case 3");
+        console.log(err);
+        console.log(user);
+        res.status(400).send(user); 
+      }
+      console.log("case 4");
+      return res.status(200).send(user);
+    });
+  })(req, res);
+});
 
 router.get('/logout', function(req, res){
   // TODO error handling here and on the front end side of this
